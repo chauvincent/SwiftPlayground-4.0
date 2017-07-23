da20dd60e4543091d2265d11844bf170027c4dbb
Vincent Chau <vincent@mightybell.com>
2017-07-23 10:05:31 -0700

    multiline strings

--- SwiftPlayground.playground/Contents.swift
+++ SwiftPlayground.playground/Contents.swift
@@ -2,6 +2,8 @@
 
 import UIKit
 
+// MARK: - Codable
+
 struct Member: Codable {
     fileprivate var _name: String
     fileprivate var _age: Int
@@ -9,7 +11,6 @@ struct Member: Codable {
     var name: String {
         return _name
     }
-
     var age: Int {
         return _age
     }
@@ -21,20 +22,30 @@ struct Member: Codable {
 }
 
 let member = Member(name: "Vince", age: 25)
+
 let encoder = JSONEncoder()
+encoder.outputFormatting = .prettyPrinted
+encoder.dateEncodingStrategy = .secondsSince1970
+
 let decoder = JSONDecoder()
+encoder.outputFormatting = .prettyPrinted
+encoder.dateEncodingStrategy = .secondsSince1970
 
 // Data -> JSON
 if let encoded = try? encoder.encode(member), let parameters = String(data: encoded, encoding: .utf8) {
     print(parameters)
-
-    // JSON -> Data
     if let decoded = try? decoder.decode(Member.self, from: encoded) {
-        print(decoded.name)
+        print(decoded.name) // JSON -> Data, no type cast needed
     }
-
 } // Parameters are now in JSON, can save or pass to a network request
 
+// MARK: - Multiline String literals
+
+let longMessageAboutPolicies = """
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas sollicitudin lacus eget porta blandit. Aliquam leo magna, fringilla at metus ut, mattis feugiat tortor. Proin aliquet, libero vel viverra facilisis, purus est congue nisi, at rhoncus justo ante ac augue. Proin sagittis suscipit sem. Aenean posuere mauris eu tincidunt tincidunt. Cras non libero sodales, efficitur orci non, suscipit turpis. In tempus magna nisl, a maximus nunc sodales a. Nam scelerisque eu orci quis lobortis. Vivamus finibus molestie diam, sit amet fringilla mi porta id. Proin varius nisl at lectus pharetra, ac porttitor nibh condimentum. Nullam a lectus euismod, efficitur urna in, luctus elit. Fusce commodo velit at enim consectetur, non congue mi rhoncus.
+"""
+
+let policyText = NSLocalizedString(longMessageAboutPolicies, comment: "Message on label about Private Policies.")
 
 
 
